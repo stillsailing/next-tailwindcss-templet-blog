ca93d21ed616226d74a15eb6cf5add60b0e08583
chenyh <chenyh.site@gmail.com>
2024-08-12 14:09:36 +0800

    worker update

--- worker/service_worker.js
+++ worker/service_worker.js
@@ -3,26 +3,22 @@ import { registerRoute } from 'workbox-routing'
 import { StaleWhileRevalidate, NetworkFirst } from 'workbox-strategies'
 
 // Precache files
-// precacheAndRoute(self.__WB_MANIFEST, {
-//   // 忽略 404 错误
-//   ignoreURLParametersMatching: [/.*/],
-//   cleanURLs: false,
-//   directoryIndex: null,
-//   urlManipulation: ({ url }) => {
-//     if (url.pathname.endsWith('/_next/app-build-manifest.json')) {
-//       return null // 忽略该文件
-//     }
-//     return [url]
-//   },
-// })
+precacheAndRoute(self.__WB_MANIFEST, {
+  urlManipulation: (url) => {
+    if (url.url.pathname.includes('/_next/app-build-manifest.json')) {
+      return null
+    }
+    return [url]
+  },
+})
 
 // Cache pages
-// registerRoute(
-//   ({ request }) => request.mode === 'navigate',
-//   new StaleWhileRevalidate({
-//     cacheName: 'pages-cache',
-//   })
-// )
+registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  new NetworkFirst({
+    cacheName: 'pages-cache',
+  })
+)
 
 // Cache static resources
 registerRoute(
@@ -30,7 +26,7 @@ registerRoute(
     request.destination === 'style' ||
     request.destination === 'script' ||
     request.destination === 'image',
-  new StaleWhileRevalidate({
+  new NetworkFirst({
     cacheName: 'static-resources-cache',
   })
 )
