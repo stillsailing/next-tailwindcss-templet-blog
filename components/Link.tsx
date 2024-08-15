b620a393940f815f040f02056f384911648e11da
chenyh <chenyh.site@gmail.com>
2024-08-15 14:24:31 +0800

    fix: lint

--- components/Link.tsx
+++ components/Link.tsx
@@ -4,21 +4,22 @@ import InternalLink from './InternalLink'
 
 const CustomLink = ({
   href,
+  children,
   ...rest
 }: LinkProps & AnchorHTMLAttributes<HTMLAnchorElement> & { viewTransitionName?: string }) => {
   const isInternalLink = href && href.startsWith('/')
   const isAnchorLink = href && href.startsWith('#')
 
   if (isInternalLink) {
-    return <InternalLink className="break-words" href={href} {...rest} />
+    return <InternalLink className="break-words" href={href} {...rest}>{children}</InternalLink>
   }
 
   if (isAnchorLink) {
-    return <a className="break-words" href={href} {...rest} />
+    return <a className="break-words" href={href} {...rest}>{children}</a>
   }
 
   return (
-    <a className="break-words" target="_blank" rel="noopener noreferrer" href={href} {...rest} />
+    <a className="break-words" target="_blank" rel="noopener noreferrer" href={href} {...rest}>{children}</a>
   )
 }
 
