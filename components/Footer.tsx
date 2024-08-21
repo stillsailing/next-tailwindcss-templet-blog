cb41b1c0ec9f22a6ce0a91cb69c5b25ca31e03a8
chenyh <chenyh.site@gmail.com>
2024-08-21 15:05:47 +0800

    feat: update header & footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,35 +1,42 @@
 import Link from './Link'
 import siteMetadata from '@/data/siteMetadata'
-import SocialIcon from '@/components/social-icons'
 import { Vercel } from '@/components/social-icons/icons'
 
 export default function Footer() {
   return (
     <footer>
       <div className="mt-16 flex flex-col items-center text-sm text-gray-500 dark:text-gray-400">
-        <div className="mb-3 flex gap-4">
-          <SocialIcon kind="mail" href={`mailto:${siteMetadata.email}`} size={6} />
-          <SocialIcon kind="github" href={siteMetadata.github} size={6} />
-          <SocialIcon kind="facebook" href={siteMetadata.facebook} size={6} />
-          <SocialIcon kind="youtube" href={siteMetadata.youtube} size={6} />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size={6} />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size={6} />
-          <SocialIcon kind="x" href={siteMetadata.x} size={6} />
-          <SocialIcon kind="instagram" href={siteMetadata.instagram} size={6} />
-          <SocialIcon kind="threads" href={siteMetadata.threads} size={6} />
-        </div>
         <div className="mb-2 flex gap-1">
           <div>{siteMetadata.headerTitle}</div>
           <div>{'©'}</div>
           <div>{new Date().getFullYear()}</div>
+          <div>{'|'}</div>
+          <a
+            className="text-sm text-primary-500 underline-offset-2 transition hover:underline"
+            target="_blank"
+            rel="noopener noreferrer"
+            href={'/feed.xml'}
+          >
+            RSS feed
+          </a>
+          <div>{'|'}</div>
+          <div>
+            Template from&nbsp;
+            <a
+              className="text-sm text-primary-500 underline-offset-2 transition hover:underline"
+              target="_blank"
+              rel="noopener noreferrer"
+              href={'https://github.com/timlrx/tailwind-nextjs-starter-blog'}
+            >
+              here
+            </a>
+          </div>
         </div>
         <div className="mb-6 flex gap-2 text-xs">
           Powered By
           <Link href="https://vercel.com" className="flex items-center gap-1 ">
             <Vercel className="h-3 w-3 fill-current" />
-            <text className="underline-offset-2 hover:text-primary-400 hover:underline">
-              Vercel
-            </text>
+            <text className="text-primary-500 underline-offset-2 hover:underline">Vercel</text>
           </Link>
         </div>
       </div>
--- components/Header.tsx
+++ components/Header.tsx
@@ -5,8 +5,6 @@ import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
 import SearchButton from './SearchButton'
-import SocialIcon from './social-icons'
-import { RSS } from './social-icons/icons'
 
 const Header = () => {
   return (
@@ -18,7 +16,7 @@ const Header = () => {
               <Logo />
             </div>
             {typeof siteMetadata.headerTitle === 'string' ? (
-              <div className="hidden h-6 text-2xl font-semibold sm:block">
+              <div className="hidden h-6 text-3xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
               </div>
             ) : (
@@ -43,16 +41,6 @@ const Header = () => {
         <SearchButton />
         <ThemeSwitch />
         <MobileNav />
-        <a
-          className="text-sm text-gray-500 transition hover:text-gray-600"
-          target="_blank"
-          rel="noopener noreferrer"
-          href={'/feed.xml'}
-        >
-          <RSS
-            className={`h-5 w-5 fill-current text-gray-700 hover:text-primary-500 dark:text-gray-200 dark:hover:text-primary-400`}
-          />
-        </a>
       </div>
     </header>
   )
