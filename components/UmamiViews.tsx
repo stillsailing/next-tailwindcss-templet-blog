249e4672ecc8a3df02829659638c14651098e20a
chenyh <chenyh.site@gmail.com>
2024-08-28 22:58:09 +0800

    chore

--- components/UmamiViews.tsx
+++ components/UmamiViews.tsx
@@ -5,15 +5,15 @@ import { useEffect, useState } from 'react'
 const Views = ({ path }) => {
   const [views, setViews] = useState(0)
 
-  useEffect(() => {
-    if (path) {
-      fetch(`/api/umami?path=${encodeURI(path)}`, { method: 'GET' })
-        .then((res) => res.json())
-        .then((result) => {
-          setViews(+result)
-        })
-    }
-  }, [path])
+  // useEffect(() => {
+  //   if (path) {
+  //     fetch(`/api/umami?path=${encodeURI(path)}`, { method: 'GET' })
+  //       .then((res) => res.json())
+  //       .then((result) => {
+  //         setViews(+result)
+  //       })
+  //   }
+  // }, [path])
 
   if (views === 0) {
     return null
