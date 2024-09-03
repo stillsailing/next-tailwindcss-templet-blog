03c97a8305e8f0d37c870b13996961b45a86e230
chenyh <chenyh.site@gmail.com>
2024-09-03 12:52:06 +0800

    chore: dependencies

--- components/Algolia.tsx
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { DocSearch, DocSearchProps } from '@docsearch/react'
-
-export default function Algolia({ appId, apiKey, indexName }: DocSearchProps) {
-  return <DocSearch appId={appId} apiKey={apiKey} indexName={indexName} />
-}
