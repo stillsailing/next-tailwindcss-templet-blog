7f68b6459776de27492cba09a3a5a81c75a21731
chenyh <chenyh.site@gmail.com>
2024-10-22 12:01:41 +0800

    data: update

--- components/Image.tsx
+++ components/Image.tsx
@@ -3,7 +3,7 @@ import NextImage, { ImageProps } from 'next/image'
 const basePath = process.env.BASE_PATH
 
 const Image = ({ src, ...rest }: ImageProps) => (
-  <NextImage src={`${basePath || ''}${src}`} {...rest} />
+  <NextImage src={`${basePath || ''}${src}`} {...rest} className="rounded" />
 )
 
 export default Image
